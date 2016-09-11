47a281194e8ccf0be5d67435149ccb4b32a1c862
Blake Rain <blake.rain@inchora.com>
2016-09-11 14:11:25 +0100

    Removed transport
    Signed-off-by: Blake Rain <blake.rain@inchora.com>

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var smtpConfig = {
   }
 };
 
-var mailtransport = nodemailer.createTransport (smtpConfig);
+//var mailtransport = nodemailer.createTransport (smtpConfig);
 
 function layout (body) {
   return [
